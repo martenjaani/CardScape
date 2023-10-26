b0d57d03143e6d825c179938313189ad2a493773
Arthur <116277145+karlkade@users.noreply.github.com>
2023-10-26 23:34:07 +0300

    Kaardisüsteemi veits parandatud

--- Assets/Scripts/AvaibleCardsScript.cs
+++ Assets/Scripts/AvaibleCardsScript.cs
@@ -1,15 +1,46 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class AvaibleCardsScript : MonoBehaviour
 {
-    List<RectTransform> Cards = new List<RectTransform>();
+    List<RectTransform> CardPacks = new List<RectTransform>();
+    List<CardScript> Cards = new List<CardScript>();
+
+    public CardScript CardPrefab;
+
+    private int amountOfFirstCards = 3;
+    private int amountOfSecondCards = 2;
+
+    public CardData FirstCardData;
+    public CardData SecondCardData;
+
     void Start()
     {
-        foreach (RectTransform card in transform) //Lisab kõik hetkel selle küljes olevad kaardid listi
+        foreach (RectTransform cardPacks in transform) //Lisab kõik hetkel selle küljes olevad kaardid listi
         {
+            CardPacks.Add(cardPacks);
+        }
+
+        if (CardPacks.Count == 2)
+        {
+            RectTransform cardPack = CardPacks[0];
+            TextMeshProUGUI[] texts = cardPack.GetComponentsInChildren<TextMeshProUGUI>();
+            texts[0].text = "Q";
+            texts[1].text = amountOfFirstCards.ToString();
+            CardScript card = Instantiate(CardPrefab, CardPacks[0]);
             Cards.Add(card);
+            card.cardData = FirstCardData;
+
+            RectTransform cardPack2 = CardPacks[1];
+            TextMeshProUGUI[] texts2 = cardPack2.GetComponentsInChildren<TextMeshProUGUI>();
+            texts2[0].text = "E";
+            texts2[1].text = amountOfSecondCards.ToString();
+            CardScript card2 = Instantiate(CardPrefab, CardPacks[1]);
+            Cards.Add(card2);
+            card2.cardData = SecondCardData;
         }
     }
 
@@ -17,24 +48,48 @@ public class AvaibleCardsScript : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.Q)) //Kui vajutab Q siis võtab listist esimese kaardi ning aktiveerib selles oleva Pressed funktsiooni.
         {
-            if(Cards.Count > 0)
+            if(amountOfFirstCards > 0)
             {
-                CardScript card = Cards[0].GetComponent<CardScript>();
-                if (card != null)
+                GameObject player = GameObject.FindGameObjectWithTag("Player");
+                if (player != null)
                 {
-                    card.Pressed();
+                    PlayerMovement playerMovement = player.GetComponent<PlayerMovement>();
+                    if (!playerMovement.onGround() && !playerMovement.ActivatedDoubleJump)
+                    {
+                        Cards[0].Pressed();
+                        amountOfFirstCards--;
+                        TextMeshProUGUI[] texts = CardPacks[0].GetComponentsInChildren<TextMeshProUGUI>();
+                        texts[1].text = amountOfFirstCards.ToString();
+                        if (amountOfFirstCards == 0)
+                        {
+                            foreach (RectTransform elements in CardPacks[0]) //Lisab kõik hetkel selle küljes olevad kaardid listi
+                            {
+                                Destroy(elements.gameObject);
+                            }
+                        }
+                    }
+                    else
+                        return;
                 }
+                else
+                    return;
             }
         }
 
         if (Input.GetKeyDown(KeyCode.E)) //Kui vajutab Q siis võtab listist esimese kaardi ning aktiveerib selles oleva Pressed funktsiooni.
         {
-            if (Cards.Count > 1)
+            if (amountOfSecondCards > 0)
             {
-                CardScript card = Cards[1].GetComponent<CardScript>();
-                if (card != null)
+                Cards[1].Pressed();
+                amountOfSecondCards--;
+                TextMeshProUGUI[] texts = CardPacks[1].GetComponentsInChildren<TextMeshProUGUI>();
+                texts[1].text = amountOfSecondCards.ToString();
+                if (amountOfSecondCards == 0)
                 {
-                    card.Pressed();
+                    foreach (RectTransform elements in CardPacks[1]) //Lisab kõik hetkel selle küljes olevad kaardid listi
+                    {
+                        Destroy(elements.gameObject);
+                    }
                 }
             }
         }
@@ -48,6 +103,6 @@ public class AvaibleCardsScript : MonoBehaviour
     public void RemoveCard(GameObject card)
     {
         RectTransform cardRect = card.GetComponent<RectTransform>();
-        Cards.Remove(cardRect);
+        CardPacks.Remove(cardRect);
     }
 }
