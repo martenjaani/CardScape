0f59878fca46077f36ca3bdf448849ec7a1b9f23
Marten Mathias Jaani <75548907+martenjaani@users.noreply.github.com>
2023-10-27 23:23:59 +0300

    ultra dash works (kinda )

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -195,15 +195,16 @@ public class PlayerMovement : MonoBehaviour
         }
     }
 
-    private void ultraDashCancel(bool isUltraDashing)
+    private void ultraDashCancel(bool isUltraDashingFunc)
     {
-        if (isUltraDashing)
+        if (isUltraDashingFunc)
         {
             if (Mathf.Approximately(transform.position.x, previousPosition.x))  //kui player model enam ei liigu, siis lopetab dashi
             {
                 isUltraDashing = false;
                 rb.velocity = Vector3.zero; // Stop the player when the dash is complete
                 rb.gravityScale = gravityMultiplier;
+                animator.SetBool("isDashing", false);
             }
             previousPosition = transform.position;
         }
