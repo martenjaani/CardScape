5c22c7b21a4b50fa62a6c57082dc598584327968
Karl Arthur Kade <116277145+karlkade@users.noreply.github.com>
2024-01-13 22:27:25 +0200

    NOW volume is done

--- Assets/Scripts/MenuScripts/SettingsScript.cs
+++ Assets/Scripts/MenuScripts/SettingsScript.cs
@@ -2,12 +2,19 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Settings : MonoBehaviour
 {
     public GameObject MainMenuPanel;
     
     private float Volume;
+
+    private void OnEnable()
+    {
+        Volume = PlayerPrefs.GetFloat("MasterVolume");
+        gameObject.GetComponentInChildren<Slider>().value = Volume;
+    }
 
     public void SetVolume(float volume)
     {
