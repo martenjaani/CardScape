d7901f18540bd1c775e4a94108385f6e6098d3e0
Karl Arthur Kade <116277145+karlkade@users.noreply.github.com>
2024-01-13 23:15:00 +0200

    fullscreen and resolution options

--- Assets/Scripts/MenuScripts/SettingsScript.cs
+++ Assets/Scripts/MenuScripts/SettingsScript.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -9,6 +10,29 @@ public class Settings : MonoBehaviour
     public GameObject MainMenuPanel;
     
     private float Volume;
+    private Resolution[] resolutions;
+
+    private void Start()
+    {
+        resolutions = Screen.resolutions;
+        TMP_Dropdown resolutionDropdown = gameObject.GetComponentInChildren<TMP_Dropdown>();
+        resolutionDropdown.ClearOptions();
+        List<string> resolutionList = new List<string>();
+        int currentResolutionIndex = 0;
+        int counter = 0;
+        foreach (Resolution resolution in resolutions)
+        {
+            resolutionList.Add(resolution.width + " x " + resolution.height);
+            if(resolution.width == Screen.currentResolution.width && resolution.height == Screen.currentResolution.height)
+            {
+                currentResolutionIndex = counter;
+            }
+            counter++;
+        }
+        resolutionDropdown.AddOptions(resolutionList);
+        resolutionDropdown.value = currentResolutionIndex;
+        resolutionDropdown.RefreshShownValue();
+    }
 
     private void OnEnable()
     {
@@ -31,5 +55,16 @@ public class Settings : MonoBehaviour
     public void SaveInfo()
     {
         PlayerPrefs.SetFloat("MasterVolume", Volume);
+    }
+
+    public void SetFullscreen(bool fullscreen)
+    {
+        Screen.fullScreen = fullscreen;
+    }
+
+    public void SetResolution(int index)
+    {
+        Resolution resolution = resolutions[index];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
     }
 }
