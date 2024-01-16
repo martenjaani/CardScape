e890867c12ff51f9ceb4f86c8872e36baa0ea49a
Karl Arthur Kade <116277145+karlkade@users.noreply.github.com>
2024-01-16 20:25:27 +0200

    Väiksed muudatused

--- Assets/Scripts/MenuScripts/SettingsScript.cs
+++ Assets/Scripts/MenuScripts/SettingsScript.cs
@@ -4,8 +4,6 @@ using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
-using UnityEngine.SceneManagement;
-using System.Linq;
 
 public class Settings : MonoBehaviour
 {
